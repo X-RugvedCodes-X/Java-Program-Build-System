0e7d7b787d57b82aaa0badb790ec4aad7615dcd0
X-RugvedCodes-X <rugvedraorane14@gmail.com>
2026-01-06 00:12:33 +0530

    Modify setup script

--- setup.bat
+++ setup.bat
@@ -2,12 +2,16 @@
 
 echo Cloning Java Build System...
 git clone https://github.com/X-RugvedCodes-X/Java-Program-Build-System.git .
-
+echo.
+echo Starting the Setup...
 echo.
 echo Cleaning unnecessary Git files...
 rmdir /s /q .git
 del README.md
 del .gitignore
+
+echo.
+call build
 
 echo.
 echo Setup completed successfully.
